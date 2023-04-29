1c51106b97b055e40875a8aad03227e636891481
rumia <rumia-san@outlook.com>
2023-04-29 15:32:48 +0800

    Add prototype of CGUISkinButton

---
+++ gframe/CGUISkinButton.cpp
@@ -0,0 +1,110 @@
+#include "SColor.h"
+#include "CGUISkinButton.h"
+namespace irr {
+namespace gui {
+
+CGUISkinButton* CGUISkinButton::addSkinButton(IGUIEnvironment* environment, const core::rect<s32>& rectangle, IGUIElement* parent, s32 id, const wchar_t* text, const wchar_t* tooltiptext)
+{
+	CGUISkinButton* button = new CGUISkinButton(environment, parent, id, rectangle);
+	if (text)
+		button->setText(text);
+
+	if (tooltiptext)
+		button->setToolTipText(tooltiptext);
+
+	if (parent)
+		button->drop(); // The parent will add a reference count (take the ownership).
+	return button;
+}
+
+CGUISkinButton::CGUISkinButton(IGUIEnvironment* environment, IGUIElement* parent, s32 id, core::rect<s32> rectangle)
+	:CGUIButton(environment, parent, id, rectangle)
+{
+	ButtonSkin = environment->createSkin(gui::EGST_WINDOWS_METALLIC);
+}
+
+CGUISkinButton::~CGUISkinButton()
+{
+	ButtonSkin->drop();
+}
+
+// Mainly copied/pasted from CGUIButton.cpp, just use my skin instead
+void CGUISkinButton::draw()
+{
+	if (!IsVisible)
+		return;
+
+	IGUISkin* skin = ButtonSkin;
+	video::IVideoDriver* driver = Environment->getVideoDriver();
+
+	const core::position2di spritePos = AbsoluteRect.getCenter();
+
+	if (!Pressed)
+	{
+		if (DrawBorder)
+			skin->draw3DButtonPaneStandard(this, AbsoluteRect, &AbsoluteClippingRect);
+
+		if (Image)
+		{
+			core::position2d<s32> pos = spritePos;
+			pos.X -= ImageRect.getWidth() / 2;
+			pos.Y -= ImageRect.getHeight() / 2;
+
+			driver->draw2DImage(Image,
+				ScaleImage ? AbsoluteRect :
+				core::recti(pos, ImageRect.getSize()),
+				ImageRect, &AbsoluteClippingRect,
+				0, UseAlphaChannel);
+		}
+	}
+	else
+	{
+		if (DrawBorder)
+			skin->draw3DButtonPanePressed(this, AbsoluteRect, &AbsoluteClippingRect);
+
+		if (PressedImage)
+		{
+			core::position2d<s32> pos = spritePos;
+			pos.X -= PressedImageRect.getWidth() / 2;
+			pos.Y -= PressedImageRect.getHeight() / 2;
+
+			if (Image == PressedImage && PressedImageRect == ImageRect)
+			{
+				pos.X += skin->getSize(EGDS_BUTTON_PRESSED_IMAGE_OFFSET_X);
+				pos.Y += skin->getSize(EGDS_BUTTON_PRESSED_IMAGE_OFFSET_Y);
+			}
+			driver->draw2DImage(PressedImage,
+				ScaleImage ? AbsoluteRect :
+				core::recti(pos, PressedImageRect.getSize()),
+				PressedImageRect, &AbsoluteClippingRect,
+				0, UseAlphaChannel);
+		}
+	}
+
+	if (Text.size())
+	{
+		IGUIFont* font = getActiveFont();
+
+		core::rect<s32> rect = AbsoluteRect;
+		if (Pressed)
+		{
+			rect.UpperLeftCorner.X += skin->getSize(EGDS_BUTTON_PRESSED_TEXT_OFFSET_X);
+			rect.UpperLeftCorner.Y += skin->getSize(EGDS_BUTTON_PRESSED_TEXT_OFFSET_Y);
+		}
+
+		if (font)
+			font->draw(Text.c_str(), rect,
+				skin->getColor(isEnabled() ? EGDC_BUTTON_TEXT : EGDC_GRAY_TEXT),
+				true, true, &AbsoluteClippingRect);
+	}
+
+	IGUIElement::draw();
+}
+
+IGUISkin* CGUISkinButton::getSkin()
+{
+	return ButtonSkin;
+}
+
+}
+}
---
+++ gframe/CGUISkinButton.h
@@ -0,0 +1,29 @@
+#pragma once
+
+/* Written by Rumia<rumia-san@outlook.com>
+*  A Button that has a standalone skin
+*  So You could modify the color of this button
+*  copy/paste/modify from CGUIButton.cpp
+*  NOTE: This class does NOT support SpriteBank!
+*/
+
+#include <irrlicht.h>
+#include "CGUIButton.h"
+namespace irr {
+namespace gui {
+
+	class CGUISkinButton : public CGUIButton
+	{
+	public:
+		static CGUISkinButton* addSkinButton(IGUIEnvironment* environment, const core::rect<s32>& rectangle,
+			IGUIElement* parent = nullptr, s32 id = -1, const wchar_t* text = nullptr, const wchar_t* tooltiptext = nullptr);
+		CGUISkinButton(IGUIEnvironment* environment, IGUIElement* parent, s32 id, core::rect<s32> rectangle);
+		~CGUISkinButton();
+		virtual void draw() override;
+		virtual IGUISkin* getSkin();
+	private:
+		IGUISkin* ButtonSkin;
+	};
+
+}
+}
